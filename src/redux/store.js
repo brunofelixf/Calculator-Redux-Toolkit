50478e5ea5dac352fad38a0ad56601eced3b2f25
brunofelixf <brunofelixf@gmail.com>
2022-03-15 22:26:27 -0300

    correção de erros

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import calcReducer from './calcSlice';
+import calcReducer from './calcSlice'
 
-export const store = configureStore(
+export const store = configureStore({
   reducer: {
     calc: calcReducer
   }
-)
-
+})
