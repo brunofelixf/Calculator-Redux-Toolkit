5dfa316d0473195dc87ef6882f50b7d97f08f588
brunofelixf <brunofelixf@gmail.com>
2022-03-16 22:47:04 -0300

    Mudanças no CSS

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -6,6 +6,7 @@ import {
   add, sub, mult, div
 } from '../redux/calcSlice'
 
+import './Calc.css'
 
 const Calc = () => {
   const[input1, setInput1] = useState(0);
@@ -15,25 +16,29 @@ const Calc = () => {
 
   return(
     <>
+    <div className="container">
     Input 1:
-    <input onChange={ e => setInput1(Number(e.target.value))} />
-    <br/>
-    Input 2:
-    <input onChange={ e => setInput2(Number(e.target.value))} />
-    <br/>
-    <button onClick={() => dispatch( add( {input1, input2} ))}>
-      +
-    </button>
-    <button onClick={() => dispatch( div( {input1, input2} ))}>
-      /
-    </button>
-    <button onClick={() => dispatch( mult( {input1, input2} ))}>
-      X
-    </button>
-    <button onClick={() => dispatch( sub( {input1, input2} ))}>
-      -
-    </button>
-    Result : { result.total }
+      <input onChange={ e => setInput1(Number(e.target.value))} />
+      <br />
+      Input 2:
+      <input onChange={ e => setInput2(Number(e.target.value))} />
+      <br/>
+      <br/>
+      <button onClick={() => dispatch( add( {input1, input2} ))}>
+        +
+      </button>
+      <button onClick={() => dispatch( div( {input1, input2} ))}>
+        /
+      </button>
+      <button onClick={() => dispatch( mult( {input1, input2} ))}>
+        X
+      </button>
+      <button onClick={() => dispatch( sub( {input1, input2} ))}>
+        -
+      </button>
+      <br />
+      Result : { result.total }
+    </div>
     </>
   )
 
